94f4a3fc4c731338e56e66995fc56abe2f002f81
Rafael D <rafael_dvid@hotmail.com>
2022-03-18 23:26:46 -0300

    starting to make genius game script

---
+++ script.js
@@ -0,0 +1,43 @@
+const theGame = [];
+const thePlayer = [];
+
+function buttonSetting(){
+    let buttonClicked = document.getElementById("yellowButton");
+    switch(buttonClicked.onclick){
+        case 
+    }
+}
+
+// configure the buttons to set their respective colors on color variable
+
+function colorEntry(){
+    switch(getElementById){
+
+    }
+}
+
+
+
+// that function makes the input of variable(input) on the Game array
+
+
+function arrayInput(){
+    switch(color){
+        case "Yellow":
+            theGame = theGame.push("Yellow");
+            break;
+        case "Green":
+            theGame = theGame.push("Green");
+            break;
+        case "Red":
+            theGame = theGame.push("Red");
+            break;
+        case "Blue":
+            theGame = theGame.push("Blue");
+            break;
+        default:
+            console.log("Something went horrible wrong");
+            break;
+    }
+}
+
